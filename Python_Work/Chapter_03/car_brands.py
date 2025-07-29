8a3b37690268b2194834d2b7535b488914efb03c
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-07-29 11:04:37 +0200

    chapter's commit.

---
+++ Python_Work/Chapter_03/car_brands.py
@@ -0,0 +1,4 @@
+CarBrandsAndModels = ['Audi 80 RS', 'B.M.W M5 M', 'Bentley Continental GT', 'Bugatti Chiron']
+message = f"My first car was a modest{CarBrandsAndModels[0].title()}."
+
+print(message)
