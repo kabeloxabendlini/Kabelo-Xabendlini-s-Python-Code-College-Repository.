db439de4132b3e8f028f2af0d09b2ced53b45365
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-07-29 11:23:13 +0200

    chapter's commit.

---
+++ Python_Work/Chapter_03/Extra_Work/Do_it_yourself_Simple_Message.py
@@ -0,0 +1,11 @@
+PeoplesNames = []
+
+PeoplesNames.append('Kabelo')
+PeoplesNames.append('Samkelo')
+PeoplesNames.append('Bantu')
+PeoplesNames.append('Thozamile')
+PeoplesNames.append('Domsie')
+PeoplesNames.append('Werner')
+PeoplesNames.append('Arno')
+
+print(PeoplesNames)
