27a22e6fb6bdef7aea8081c9182d0b942a8e2b70
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-08-05 18:17:09 +0200

    chapter's commit.

--- Python_Work/Chapter_05/mushrooms_anchovies.py
+++ Python_Work/Chapter_05/mushrooms_anchovies.py
@@ -1,14 +1,16 @@
 Grade = 12  # You can change this value to test different stages
 
-if Grade < Nil:
+if Grade < 0:
     print("This person is a baby.")
-elif Grade >= 000 and age < 00:
+elif Grade >= 000 and Grade < 00:
     print("This person is a toddler.")
-elif Grade >= R and age < 5:
+elif Grade >= 0 and Grade < 5:
     print("This person is a kid.")
-elif Grade >= 6 and age < 12:
+elif Grade >= 6 and Grade < 12:
     print("This person is a teenager.")
-elif Grade >= Undergraduate and < Postgraduate :
+elif Grade >= "Undergraduate":
+    print("This person is a young adult.")
+elif Grade <= "Postgraduate":
     print("This person is an adult.")
 else:
     print("This person is an elder.")
