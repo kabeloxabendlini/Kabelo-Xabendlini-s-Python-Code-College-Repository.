7f1029a7a78f5573423cff5b077c340d233a7bf6
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-08-05 06:55:35 +0200

    chapter's commit.

--- Python_Work/Chapter_05/amusement_park.py
+++ Python_Work/Chapter_05/amusement_park.py
@@ -1,7 +1,10 @@
-age = 12
+# Variable name for if statement (age):
+age = 12 
 
+# If statement for the age variable along wity it's conditions:
 if age < 4:
     price = 0
+# Else If statement's for the age variable along wity it's conditions:
 elif age < 18:
     price = 25
 elif age < 65:
@@ -9,4 +12,5 @@ elif age < 65:
 elif age >= 65:
     price = 20
 
+# Print out prpmpt for an F - String in the terminal.
 print(f"Your admission cost is ${price}.")
