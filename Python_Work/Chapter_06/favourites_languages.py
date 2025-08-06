74490c8f010719b62121881eaf256ecb96be5516
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-08-06 10:29:00 +0200

    chapter's commit.

--- Python_Work/Chapter_06/favourites_languages.py
+++ Python_Work/Chapter_06/favourites_languages.py
@@ -7,55 +7,55 @@ favorite_languages = {
 
 ##################################### 1 - 6 ###############################################
 
-favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'rust',
-    'phil': 'python',
-    }
+# favorite_languages = {
+#     'jen': 'python',
+#     'sarah': 'c',
+#     'edward': 'rust',
+#     'phil': 'python',
+#     }
 
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+# language = favorite_languages['sarah'].title()
+# print(f"Sarah's favorite language is {language}.")
 
 ##################################### 10 - 18 ###############################################
 
-favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'rust',
-    'phil': 'python',
-    }
+# favorite_languages = {
+#     'jen': 'python',
+#     'sarah': 'c',
+#     'edward': 'rust',
+#     'phil': 'python',
+#     }
 
-for name, language in favorite_languages.items():
-    print(f"{name.title()}'s favorite language is {language.title()}.")
+# for name, language in favorite_languages.items():
+#     print(f"{name.title()}'s favorite language is {language.title()}.")
 
 ##################################### 22 - 30 ###############################################
 
-favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'rust',
-    'phil': 'python',
-    }
+# favorite_languages = {
+#     'jen': 'python',
+#     'sarah': 'c',
+#     'edward': 'rust',
+#     'phil': 'python',
+#     }
 
-friends = ['phil', 'sarah']
-for name in favorite_languages.keys():
-    print(f"Hi {name.title()}.")
+# friends = ['phil', 'sarah']
+# for name in favorite_languages.keys():
+#     print(f"Hi {name.title()}.")
     
-    if name in friends:
-        language = favorite_languages[name].title()
-        print(f"\t{name.title()}, I see you love {language}!")
+#     if name in friends:
+#         language = favorite_languages[name].title()
+#         print(f"\t{name.title()}, I see you love {language}!")
 
 ##################################### 34 - 47 ###############################################
 
-favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'rust',
-    'phil': 'python',
-    }
+# favorite_languages = {
+#     'jen': 'python',
+#     'sarah': 'c',
+#     'edward': 'rust',
+#     'phil': 'python',
+#     }
 
-if 'erin' not in favorite_languages.keys():
-    print("Erin, please take our poll!")
+# if 'erin' not in favorite_languages.keys():
+#     print("Erin, please take our poll!")
 
 ##################################### 51 - 59 ################################################
