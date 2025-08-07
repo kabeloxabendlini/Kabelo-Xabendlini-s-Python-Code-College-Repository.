53c8bfbe402e98a669a6b15172060d8d6de09fb2
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-08-07 09:48:48 +0200

    chapter's commit.

--- Python_Work/Chapter_07/parrot.py
+++ Python_Work/Chapter_07/parrot.py
@@ -12,7 +12,7 @@ while active:
 
 #################################### Line 1 - 11 ###############################
 
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
 
 #################################### Line 15 - 16 ###############################
