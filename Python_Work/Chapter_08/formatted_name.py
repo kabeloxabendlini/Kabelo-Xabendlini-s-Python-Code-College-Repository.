4f5888af1b24f6964eea42fa1dbdffa1753381d0
kabeloxabendlini <kabeloxabendlini385@gmail.com>
2025-08-14 07:30:22 +0200

    chapter's commit.

--- Python_Work/Chapter_08/formatted_name.py
+++ Python_Work/Chapter_08/formatted_name.py
@@ -14,13 +14,13 @@ print(musician)
 
 ######################################### Line 1 - 13 #######################################
 
-# def get_formatted_name(first_name, last_name):
-#     """Return a full name, neatly formatted."""
-#     full_name = f"{first_name} {last_name}"
-#     return full_name.title()
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
-# musician = get_formatted_name('jimi', 'hendrix')
-# print(musician)
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
 
 ############################################ Line 17 - 23 #######################################
 
