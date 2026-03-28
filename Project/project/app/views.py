9145fee0438d9f94feebd4c6fbfee1e9c26a0b1b
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-28 20:45:13 +0200

    fix: remove env, node_modules and sqlite from tracking

--- Project/project/app/views.py
+++ Project/project/app/views.py
@@ -1,36 +1,43 @@
 # app - views.py file
 
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render,redirect
 from .models import User
 from .forms import UserForm
+# Create your views here.
 
 def user_list(request):
-    records = User.objects.all()
-    mydict = {'records': records}
-    return render(request, 'Listingpage.html', context=mydict)
+    records=User.objects.all()
+    mydict={'records':records}
+    return render(request,'Listingpage.html',context=mydict)
 
 def AddUser(request):
-    form = UserForm(request.POST or None, request.FILES or None)
+    mydict={}
+    form=UserForm(request.POST or None , request.FILES or None)
     if form.is_valid():
         form.save()
-        return redirect('user-list')
-    return render(request, 'Add.html', {'form': form})
+        return redirect('/')
 
-def EditUser(request, id=None):
-    one_rec = get_object_or_404(User, pk=id)
-    form = UserForm(request.POST or None, request.FILES or None, instance=one_rec)
+    mydict['form']=form
+    return render(request,'Add.html',mydict)
+
+def EditUser(request,id=None):
+    one_rec=User.objects.get(pk=id)
+    form=UserForm(request.POST or None,request.FILES or None, instance=one_rec)
     if form.is_valid():
         form.save()
-        return redirect('user-list')
-    return render(request, 'Edit.html', {'form': form})
+        return redirect('/')
+    mydict= {'form':form}
+    return render(request,'Edit.html',context=mydict)
 
-def DeleteUser(request, eid=None):
-    one_rec = get_object_or_404(User, pk=eid)
-    if request.method == "POST":
-        one_rec.delete()
-        return redirect('user-list')
-    return render(request, 'Delete.html', {'user': one_rec})
+def DeleteUser(request,eid=None):
+    one_rec = User.objects.get(pk=eid)
+    if  request.method=="POST":
+         one_rec.delete()
+         return redirect('/')
+    return render(request,'Delete.html')
 
-def ViewUser(request, eid=None):
-    one_rec = get_object_or_404(User, pk=eid)
-    return render(request, 'View.html', {'user': one_rec})
+def ViewUser(request,eid=None):
+    mydict={}
+    one_rec = User.objects.get(pk=eid)
+    mydict['user']=one_rec
+    return render(request,'''View.html''',mydict)
