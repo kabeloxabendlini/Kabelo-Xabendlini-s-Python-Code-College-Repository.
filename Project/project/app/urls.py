7becd2312b83f52e86a046b47ef14d0a3dda9edb
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-28 19:43:35 +0200

    fix: remove env, node_modules and sqlite from tracking

--- Project/project/app/urls.py
+++ Project/project/app/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_list),
-    path('Add/', views.AddUser),
-    path('Edit/<id>', views.EditUser),
-    path('Delete/<eid>', views.DeleteUser),
-    path('View/<eid>', views.ViewUser),
+    path('', views.user_list, name='user-list'),
+    path('Add/', views.AddUser, name='add-user'),
+    path('Edit/<int:id>/', views.EditUser, name='edit-user'),
+    path('Delete/<int:eid>/', views.DeleteUser, name='delete-user'),
+    path('View/<int:eid>/', views.ViewUser, name='view-user'),
 ]
--- Project/project/app/views.py
+++ Project/project/app/views.py
@@ -1,43 +1,36 @@
 # app - views.py file
 
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import User
 from .forms import UserForm
-# Create your views here.
 
 def user_list(request):
-    records=User.objects.all()
-    mydict={'records':records}
-    return render(request,'Listingpage.html',context=mydict)
+    records = User.objects.all()
+    mydict = {'records': records}
+    return render(request, 'Listingpage.html', context=mydict)
 
 def AddUser(request):
-    mydict={}
-    form=UserForm(request.POST or None , request.FILES or None)
+    form = UserForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
-        return redirect('/')
+        return redirect('user-list')
+    return render(request, 'Add.html', {'form': form})
 
-    mydict['form']=form
-    return render(request,'Add.html',mydict)
-
-def EditUser(request,id=None):
-    one_rec=User.objects.get(pk=id)
-    form=UserForm(request.POST or None,request.FILES or None, instance=one_rec)
+def EditUser(request, id=None):
+    one_rec = get_object_or_404(User, pk=id)
+    form = UserForm(request.POST or None, request.FILES or None, instance=one_rec)
     if form.is_valid():
         form.save()
-        return redirect('/')
-    mydict= {'form':form}
-    return render(request,'Edit.html',context=mydict)
+        return redirect('user-list')
+    return render(request, 'Edit.html', {'form': form})
 
-def DeleteUser(request,eid=None):
-    one_rec = User.objects.get(pk=eid)
-    if  request.method=="POST":
-         one_rec.delete()
-         return redirect('/')
-    return render(request,'Delete.html')
+def DeleteUser(request, eid=None):
+    one_rec = get_object_or_404(User, pk=eid)
+    if request.method == "POST":
+        one_rec.delete()
+        return redirect('user-list')
+    return render(request, 'Delete.html', {'user': one_rec})
 
-def ViewUser(request,eid=None):
-    mydict={}
-    one_rec = User.objects.get(pk=eid)
-    mydict['user']=one_rec
-    return render(request,'''View.html''',mydict)
+def ViewUser(request, eid=None):
+    one_rec = get_object_or_404(User, pk=eid)
+    return render(request, 'View.html', {'user': one_rec})
