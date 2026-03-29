9e151342e32d43e1e2a862b1c64d0364aae63c63
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-29 20:15:33 +0200

    Implemented Cloudinary, Multer, auth, and upload system

--- Project/project/project/settings.py
+++ Project/project/project/settings.py
@@ -63,8 +63,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3')
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
